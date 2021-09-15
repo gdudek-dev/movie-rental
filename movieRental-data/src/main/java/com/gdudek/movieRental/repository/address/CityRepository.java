a7825717b10a98ec2c3223c70e2004a6d98a2665
Grzegorz Dudek <74261917+MeZziK@users.noreply.github.com>
2021-09-15 20:26:37 +0200

    delete method

--- movieRental-data/src/main/java/com/gdudek/movieRental/repository/address/CityRepository.java
+++ movieRental-data/src/main/java/com/gdudek/movieRental/repository/address/CityRepository.java
@@ -5,14 +5,12 @@ import com.gdudek.movieRental.model.address.City;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CityRepository extends JpaRepository<City,Long> {
 
     Optional<City> findCityByName(String name);
-    Optional<List<City>> findAllByCountry_Name(String name);
 
     boolean existsByName(String name);
 
