e7385ae565aa2cd58b226a741386b438c039e658
Grzegorz Dudek <74261917+MeZziK@users.noreply.github.com>
2021-10-13 20:39:06 +0200

    fix problem with ArrayIndexOutOfBounds

--- movieRental-data/src/main/java/com/gdudek/movieRental/utils/ConvertStringToCaseSensitive.java
+++ movieRental-data/src/main/java/com/gdudek/movieRental/utils/ConvertStringToCaseSensitive.java
@@ -4,14 +4,21 @@ public class ConvertStringToCaseSensitive {
 
     public static String getConvertedString(String stringToConvert)
     {
-        String convertedString="";
 
-        String[] splitted = stringToConvert.split(" ");
+        if(stringToConvert!="") {
+            String convertedString = "";
 
-        for(int i =0 ; i< splitted.length;i++){
-            convertedString+=splitted[i].substring(0,1).toUpperCase()+splitted[i].substring(1).toLowerCase()+" ";
+            String[] splitted = stringToConvert.split(" ");
+
+            for (int i = 0; i < splitted.length; i++) {
+                if (i != 0) {
+                    convertedString += " ";
+                }
+                convertedString += splitted[i].substring(0, 1).toUpperCase() + splitted[i].substring(1).toLowerCase();
+            }
+
+            return convertedString;
         }
-
-        return convertedString;
+        return "";
     }
 }
